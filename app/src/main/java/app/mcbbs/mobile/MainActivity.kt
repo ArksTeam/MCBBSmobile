c62f9cbcfa6820920c099c2f6b7d7798f6f70802
JankieQwQ <qwq@awaish.link>
2024-03-17 17:21:01 +0800

    Initial commit

---
+++ app/src/main/java/app/mcbbs/mobile/MainActivity.kt
@@ -0,0 +1,46 @@
+package app.mcbbs.mobile
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import app.mcbbs.mobile.ui.theme.MCBBSTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MCBBSTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    WebViewContainer()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WebViewContainer() {
+    AndroidView(factory = { context ->
+        val webView = WebView(context)
+        webView.loadUrl("https://mcbbs.app")
+        webView
+    }, modifier = Modifier.fillMaxSize())
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WebViewContainerPreview() {
+    MCBBSTheme {
+        WebViewContainer()
+    }
+}

b5a5b6c480d9ceba5d540743ab4a5c383d00fab3
JankieQwQ <qwq@awaish.link>
2024-03-17 17:40:08 +0800

    [debug] Import webview

--- app/src/main/java/app/mcbbs/mobile/MainActivity.kt
+++ app/src/main/java/app/mcbbs/mobile/MainActivity.kt
@@ -2,6 +2,7 @@ package app.mcbbs.mobile
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
